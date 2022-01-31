a6b69ef0c5cb7c59300fa884bbd353d3727f91d9
Gianluca Benassi <benox1199@gmail.com>
2022-01-31 15:06:03 +0100

    Product shipment price

--- classes/Products.php
+++ classes/Products.php
@@ -4,16 +4,43 @@ class Products {
     protected $name;
     protected $desc;
     protected $price;
-    protected $shipmentDay;
     protected $vendor;
+    protected $shipmentDay;
+    protected $shipmentPrice;
 
-    function __construct($_name, $_desc, $_price, $_shipment, $_vendor) {
+    function __construct($_name, $_desc, $_price, $_vendor, $_shipmentDay, $_shipmentPrice) {
         $this->name = $_name;
         $this->desc = $_desc;
         $this->price = $_price;
-        $this->shipment = $_shipment;
         $this->vendor = $_vendor;
+        $this->shipmentDay = $_shipmentDay;
     }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getDesc() {
+        return $this->desc;
+    }
+
+    function getPrice() {
+        return $this->price;
+    }
+
+    function getVendor() {
+        return $this->vendor;
+    }
+    
+    function getShipmentDay() {
+        return $this->ShipmentDay;
+    }
+
+    function getShipmentPrice() {
+        return number_format($this->price * 10 / 100, 2);
+    }
+
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 include_once __DIR__ . "./classes/Products.php";
 
+$test = new Products("prodotto1", "questo è un bel prodotto", 150.99, "venditore1", 20,10);
+
+var_dump($test);
+
+echo $test->getShipmentPrice();
+
 ?>
